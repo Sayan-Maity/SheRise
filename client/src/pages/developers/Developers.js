3f6b4399871c630a3a3bd6494a084ba528461b35
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-12 17:43:29 +0530

    developer of this SheRise website

---
+++ client/src/pages/developers/Developers.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Footer from '../../components/footer/Footer'
+import Navbar from '../../components/navbar/Navbar'
+import '../../components/sidebar/Sidebar.css'
+import './Developers.css'
+
+const Developers = () => {
+  return (
+    <>
+      <div className='developers'>
+        <Navbar/>
+        <div className="heading">
+          <p>Meet Our Team</p>
+          <p>Developers</p>
+          </div>
+        <section className='developer-profile'>
+          
+          <div className="outer-card">
+            <div className="image"> 
+              <div className="avatar sayan-avatar"></div>
+              <div className="avatar-overlay"></div>
+              <div className="avatar-overlay2"></div>
+            </div>
+            <div className="description">
+              <div className="name">
+                <p> Sayan Maity</p>
+                <p>Full-Stack Developer</p>
+              </div>
+              <div className="buttons">
+                <a href="https://twitter.com/Sayancr777" target="_blank"><p>Twitter</p></a>
+                <a href="https://www.linkedin.com/in/sayan-maity2003/" target="_blank"><p>Linkedin</p></a>
+              </div>
+            </div>
+          </div>
+          <div className="outer-card">
+            <div className="image"> 
+              <div className="avatar shrestha-avatar"></div>
+              <div className="avatar-overlay"></div>
+              <div className="avatar-overlay2"></div>
+            </div>
+            <div className="description">
+              <div className="name">
+                <p> Shreshtha Das</p>
+                <p>UI/UX Designer</p>
+              </div>
+              <div className="buttons">
+                <a href="https://twitter.com/classyvioletta" target="_blank"><p>Twitter</p></a>
+                <a href="https://www.linkedin.com/in/shrestha-das-09365b260/" target="_blank"><p>Linkedin</p></a>
+              </div>
+            </div>
+          </div>
+
+        </section>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default Developers
