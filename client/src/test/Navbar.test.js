35304d749e46ffa56c18102ea81099642461a9a6
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-15 21:10:11 +0530

    written tests for Navbar coponent

---
+++ client/src/test/Navbar.test.js
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../components/navbar/Navbar";
+import { MemoryRouter } from "react-router-dom";
+
+// Helper function to render the Register component with MemoryRouter
+const renderRegister = () => {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+};
+
+describe("Component Navbar Test", () => {
+  it("Home NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "HOME" });
+    expect(navLinkElement.textContent).toBe("HOME");
+    expect(navLinkElement.getAttribute("href")).toBe("/");
+  });
+  
+  it("Scholarship NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "SCHOLARSHIP" });
+    expect(navLinkElement.textContent).toBe("SCHOLARSHIP");
+    expect(navLinkElement.getAttribute("href")).toBe("/scholarship");
+  });
+
+  it("Developers NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "DEVELOPERS" });
+    expect(navLinkElement.textContent).toBe("DEVELOPERS");
+    expect(navLinkElement.getAttribute("href")).toBe("/developers");
+  });
+
+  it("Contact Us NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "CONTACT US" });
+    expect(navLinkElement.textContent).toBe("CONTACT US");
+    expect(navLinkElement.getAttribute("href")).toBe("/contact");
+  });
+
+  it("Login NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "LOGIN" });
+    expect(navLinkElement.textContent).toBe("LOGIN");
+    expect(navLinkElement.getAttribute("href")).toBe("/login");
+  });
+
+  it("Register NavLink should be rendered with the correct text and destination", () => {
+    renderRegister();
+    const navLinkElement = screen.getByRole("link", { name: "REGISTER" });
+    expect(navLinkElement.textContent).toBe("REGISTER");
+    expect(navLinkElement.getAttribute("href")).toBe("/register");
+  });
+
+});
