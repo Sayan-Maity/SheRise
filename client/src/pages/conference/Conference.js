a5fa125b437f7bd464f62a42bc97af0dfab47204
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-18 05:15:39 +0530

    formated the document and removed unused spaces and indentations

--- client/src/pages/conference/Conference.js
+++ client/src/pages/conference/Conference.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 
 import "./Conference.css";
 import ConferenceCard from "../../components/cards/conferencecard/ConferenceCard";
-import ConferenceBanner from "../../assets/svg/type3.svg"
-import ConferenceBanner2 from "../../assets/svg/conferenceBanner.svg"
+import ConferenceBanner from "../../assets/svg/type3.svg";
+import ConferenceBanner2 from "../../assets/svg/conferenceBanner.svg";
 import { Helmet } from "react-helmet";
 
 const Conference = () => {
@@ -14,7 +14,9 @@ const Conference = () => {
   useEffect(() => {
     async function call() {
       try {
-        let resp = await Axios.get(`${process.env.REACT_APP_SERVER_URL}/conferences`);
+        let resp = await Axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/conferences`
+        );
         setConferences(resp.data);
         // console.log(resp.data);
       } catch (err) {
@@ -23,20 +25,19 @@ const Conference = () => {
     }
     call();
   }, []);
-  
+
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
-  }
+  };
 
-  const filteredConferences = conferences.filter(conference => {
+  const filteredConferences = conferences.filter((conference) => {
     return conference.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
   return (
-  <>
-
-    <div className="conference">
-    <Helmet>
+    <>
+      <div className="conference">
+        <Helmet>
           <title>SheRise | Conferences</title>
           <meta
             name="description"
@@ -47,59 +48,80 @@ const Conference = () => {
             content="Conference, Career, Tech, Opportunities, Job Listings"
           />
         </Helmet>
-      <div className="conference-banner-section">
-        <div className="left">
-          <div className="left-image"><img src={ConferenceBanner2} alt={ConferenceBanner2} className="conference-banner-image" /></div>
-          <div className="conference-heading"> <p> Conferences</p></div>
-        </div>
-        <div className="right">
-          <div className="left-image"><img src={ConferenceBanner} alt={ConferenceBanner} className="conference-banner-image" /></div>
-
-        </div>
-      </div>
-      <div className="searchbar-card-opportunity">
-        <div className="searchbar-card">
-          <div className='conference-searchbar'>
-            <input type="text" placeholder="Search Conferences" onChange={handleSearch} />
-            <div className="search-btn"><p>Search</p></div>
-          </div>
-          <section className="conferences-con">
-            {filteredConferences.map((conference) => {
-              return <ConferenceCard conference={conference} />;
-            })}
-          </section>
-        </div>
-        <div className="opportunity">
-          <div className="opportunity-card">
-            <div className="opportunity-heading">
-              <p>Featured Opportunities</p>
+        <div className="conference-banner-section">
+          <div className="left">
+            <div className="left-image">
+              <img
+                src={ConferenceBanner2}
+                alt={ConferenceBanner2}
+                className="conference-banner-image"
+              />
             </div>
-            <div className="opportunity-list">
-              <div className="opportunity-list1">
-                <div className="left opportunity-image1"></div>
-                <div className="right">
-                  <p>L'Oréal Brandstorm 2023 - PPO & PPI's up for grab</p>
-                </div>
-              </div>
-              <div className="opportunity-list1">
-                <div className="left opportunity-image2"></div>
-                <div className="right">
-                  <p>Women Code to Win 2023 - India | PPIs & ₹4,50,000 in prizes!</p>
-                </div>
-              </div>
-              <div className="opportunity-list1">
-                <div className="left opportunity-image3"></div>
-                <div className="right">
-                  <p>IIM Lucknow's Manfest-Varchasva 2022-23</p>
-                </div>
-              </div>
-
+            <div className="conference-heading">
+              {" "}
+              <p> Conferences</p>
+            </div>
+          </div>
+          <div className="right">
+            <div className="left-image">
+              <img
+                src={ConferenceBanner}
+                alt={ConferenceBanner}
+                className="conference-banner-image"
+              />
             </div>
           </div>
         </div>
+        <div className="searchbar-card-opportunity">
+          <div className="searchbar-card">
+            <div className="conference-searchbar">
+              <input
+                type="text"
+                placeholder="Search Conferences"
+                onChange={handleSearch}
+              />
+              <div className="search-btn">
+                <p>Search</p>
+              </div>
+            </div>
+            <section className="conferences-con">
+              {filteredConferences.map((conference) => {
+                return <ConferenceCard conference={conference} />;
+              })}
+            </section>
+          </div>
+          <div className="opportunity">
+            <div className="opportunity-card">
+              <div className="opportunity-heading">
+                <p>Featured Opportunities</p>
+              </div>
+              <div className="opportunity-list">
+                <div className="opportunity-list1">
+                  <div className="left opportunity-image1"></div>
+                  <div className="right">
+                    <p>L'Oréal Brandstorm 2023 - PPO & PPI's up for grab</p>
+                  </div>
+                </div>
+                <div className="opportunity-list1">
+                  <div className="left opportunity-image2"></div>
+                  <div className="right">
+                    <p>
+                      Women Code to Win 2023 - India | PPIs & ₹4,50,000 in
+                      prizes!
+                    </p>
+                  </div>
+                </div>
+                <div className="opportunity-list1">
+                  <div className="left opportunity-image3"></div>
+                  <div className="right">
+                    <p>IIM Lucknow's Manfest-Varchasva 2022-23</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-
     </>
   );
 };
