bbe844feea6a5c85ea622f3e731ce680393672de
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-18 05:27:44 +0530

    formated the document and removed unused spaces and indentations

--- client/src/pages/hackathon/Hackathon.js
+++ client/src/pages/hackathon/Hackathon.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
-import './Hackathon.css';
-import { SiPluscodes } from 'react-icons/si';
-import HackathonBanner from "../../assets/svg/type3.svg"
-import HackathonBanner2 from "../../assets/svg/hackathonBanner3.svg"
-import HackathonCard from '../../components/cards/hackathoncard/HackathonCard';
-import { Helmet } from 'react-helmet';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./Hackathon.css";
+import { SiPluscodes } from "react-icons/si";
+import HackathonBanner from "../../assets/svg/type3.svg";
+import HackathonBanner2 from "../../assets/svg/hackathonBanner3.svg";
+import HackathonCard from "../../components/cards/hackathoncard/HackathonCard";
+import { Helmet } from "react-helmet";
 
 const Hackathon = () => {
   const [hackathons, setHackathons] = useState([]);
@@ -14,7 +14,9 @@ const Hackathon = () => {
   useEffect(() => {
     async function call() {
       try {
-        let resp = await Axios.get(`${process.env.REACT_APP_SERVER_URL}/hackathons` );
+        let resp = await Axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/hackathons`
+        );
         setHackathons(resp.data);
       } catch (err) {
         console.log(err);
@@ -25,40 +27,60 @@ const Hackathon = () => {
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
-  }
+  };
 
-  const filteredHackathons = hackathons.filter(hackathon => {
+  const filteredHackathons = hackathons.filter((hackathon) => {
     return hackathon.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
   return (
     <div className="hackathon">
       <Helmet>
-          <title>SheRise | Hackathons</title>
-          <meta
-            name="description"
-            content="SheRise's Hackathon Page. Here all the job listing related to the Hackathon tags are dynamically listed down here. "
-          />
-          <meta
-            name="keywords"
-            content="Hackathon, Career, Tech, Opportunities, Job Listings"
-          />
-        </Helmet>
+        <title>SheRise | Hackathons</title>
+        <meta
+          name="description"
+          content="SheRise's Hackathon Page. Here all the job listing related to the Hackathon tags are dynamically listed down here. "
+        />
+        <meta
+          name="keywords"
+          content="Hackathon, Career, Tech, Opportunities, Job Listings"
+        />
+      </Helmet>
       <div className="hackathon-banner-section">
         <div className="left">
-          <div className="left-image"><img src={HackathonBanner2} alt={HackathonBanner2} className="hackathon-banner-image" /></div>
-          <div className="hackathon-heading"> <p> Hackathons</p></div>
+          <div className="left-image">
+            <img
+              src={HackathonBanner2}
+              alt={HackathonBanner2}
+              className="hackathon-banner-image"
+            />
+          </div>
+          <div className="hackathon-heading">
+            {" "}
+            <p> Hackathons</p>
+          </div>
         </div>
         <div className="right">
-          <div className="left-image"><img src={HackathonBanner} alt={HackathonBanner} className="hackathon-banner-image" /></div>
-
+          <div className="left-image">
+            <img
+              src={HackathonBanner}
+              alt={HackathonBanner}
+              className="hackathon-banner-image"
+            />
+          </div>
         </div>
       </div>
       <div className="searchbar-card-opportunity">
         <div className="searchbar-card">
-          <div className='hackathon-searchbar'>
-            <input type="text" placeholder="Search Hackathons" onChange={handleSearch} />
-            <div className="search-btn"><p>Search</p></div>
+          <div className="hackathon-searchbar">
+            <input
+              type="text"
+              placeholder="Search Hackathons"
+              onChange={handleSearch}
+            />
+            <div className="search-btn">
+              <p>Search</p>
+            </div>
           </div>
           <section className="hackathons-con">
             {filteredHackathons.map((hackathon) => {
@@ -81,7 +103,9 @@ const Hackathon = () => {
               <div className="opportunity-list1">
                 <div className="left opportunity-image2"></div>
                 <div className="right">
-                  <p>Women Code to Win 2023 - India | PPIs & ₹4,50,000 in prizes!</p>
+                  <p>
+                    Women Code to Win 2023 - India | PPIs & ₹4,50,000 in prizes!
+                  </p>
                 </div>
               </div>
               <div className="opportunity-list1">
@@ -90,7 +114,6 @@ const Hackathon = () => {
                   <p>IIM Lucknow's Manfest-Varchasva 2022-23</p>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
