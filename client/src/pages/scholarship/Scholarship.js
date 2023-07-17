d54a4107b327e6453b0d5fd31e37d3b48ebaddf0
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-18 04:37:04 +0530

    added meta tags to the child scholarship component for SEO

--- client/src/pages/scholarship/Scholarship.js
+++ client/src/pages/scholarship/Scholarship.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import Navbar from "../../components/navbar/Navbar";
-import './Scholarship.css'
+import "./Scholarship.css";
 import DonateBanner from "../../assets/svg/Donation.svg";
 import { BsArrowRight } from "react-icons/bs";
 import Footer from "../../components/footer/Footer";
 import ReactGA from "react-ga4";
+import { Helmet } from "react-helmet";
 
 let stripePromise;
 
@@ -23,14 +24,14 @@ const Scholarship = () => {
   const [isLoading, setLoading] = useState(false);
   const item = {
     price: process.env.REACT_APP_PRODUCT_ID,
-    quantity: 1
+    quantity: 1,
   };
 
   const checkoutOptions = {
     lineItems: [item],
     mode: "payment",
     successUrl: `${window.location.origin}/success`,
-    cancelUrl: `${window.location.origin}/cancel`
+    cancelUrl: `${window.location.origin}/cancel`,
   };
 
   const redirectToCheckout = async () => {
@@ -49,72 +50,147 @@ const Scholarship = () => {
 
   return (
     <>
-    <Navbar/>
-    <div className="scholarship">
-      <div className="heading-scholarship">
-        <div className="top-heading">
-          <div className="overlay-donate"><p>Donate</p></div>
-          <div className="main-scholarship-heading">
-            <p>Be the reason </p>
-            <p>someone smiles today !</p>
+      <Navbar />
+      <Helmet>
+        <title>SheRise | Scholarship</title>
+        <meta
+          name="description"
+          content="SheRise provides Scholarship to under privileged girls and also we are raising funds for the same."
+        />
+        <meta
+          name="keywords"
+          content="Fund Raise, Donate, Support SheRise, Scholarship, Career, Tech, Opportunities, Job Listings"
+        />
+      </Helmet>
+      <div className="scholarship">
+        <div className="heading-scholarship">
+          <div className="top-heading">
+            <div className="overlay-donate">
+              <p>Donate</p>
+            </div>
+            <div className="main-scholarship-heading">
+              <p>Be the reason </p>
+              <p>someone smiles today !</p>
+            </div>
+          </div>
+          <div className="bottom-description">
+            <p>
+              We at SheRise want to give the opportunity to all the
+              underprivileged girls, to come and explore the STEM field and
+              excel their career in tech. Donate now and help to shape their
+              futures!
+            </p>
+          </div>
+          <div className="checkout">
+            <button
+              className="checkout-button"
+              onClick={redirectToCheckout}
+              disabled={isLoading}
+            >
+              <div className="donate-btn">
+                <p className="checkout-btn-text">
+                  {isLoading ? "Loading..." : "Donate Now"}
+                </p>
+              </div>
+              <div className="purple-circle">
+                <BsArrowRight className="arrow-icon" />
+              </div>
+            </button>
           </div>
         </div>
-        <div className="bottom-description">
-          <p>We at SheRise want to give the opportunity to all the underprivileged girls, to come and explore the STEM field and excel their career in tech. Donate now and help to shape their futures!</p>
-        </div>
-        <div className="checkout">      
-          <button
-            className="checkout-button"
-            onClick={redirectToCheckout}
-            disabled={isLoading}
-          >
-            <div className="donate-btn">
-              <p className="checkout-btn-text">{isLoading ? "Loading..." : "Donate Now"}</p>
-            </div>
-            <div className="purple-circle">
-              <BsArrowRight className="arrow-icon"/>
-            </div>
-          </button>
+        <div className="scholarship-banner">
+          <img src={DonateBanner} alt="DonateBanner" draggable="false" />
         </div>
       </div>
-      <div className="scholarship-banner"><img src={DonateBanner} alt="DonateBanner" draggable="false"/></div>
-    </div>
 
-    <div className="poor-girl">
-      <div className="poor-girl-section">
-        <div className="poor-girl-section-main">
-          <div className="desc">
-            <div className="heading">
-              <p></p>
-            </div>
-            <div className="scholarship-desc">
-              <p></p>
+      <div className="poor-girl">
+        <div className="poor-girl-section">
+          <div className="poor-girl-section-main">
+            <div className="desc">
+              <div className="heading">
+                <p></p>
+              </div>
+              <div className="scholarship-desc">
+                <p></p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="scholarship-form">
-        <div className="scholarship-form-desc">
-          <p>Scholarship Form</p>
-          <p>*Fill the below form to avail for the scholarship</p>
+        <div className="scholarship-form">
+          <div className="scholarship-form-desc">
+            <p>Scholarship Form</p>
+            <p>*Fill the below form to avail for the scholarship</p>
+          </div>
+          <div className="contactFormContainer">
+            <form action="https://formspree.io/f/xgebelva" method="POST">
+              <div className="name">
+                <label for="text">
+                  {" "}
+                  <input
+                    type="text"
+                    name="Name"
+                    id="name"
+                    placeholder="First Name*"
+                    required
+                  />
+                </label>
+                <label for="text">
+                  {" "}
+                  <input
+                    type="text"
+                    name="Name"
+                    id="name"
+                    placeholder="Last Name*"
+                    required
+                  />
+                </label>
+              </div>
+              <label for="email">
+                {" "}
+                <input
+                  type="email"
+                  name="_replyto"
+                  id="email"
+                  placeholder="Your Email*"
+                  required
+                />{" "}
+              </label>
+              <label for="text">
+                {" "}
+                <input
+                  type="text"
+                  name="_replyto"
+                  id="board"
+                  placeholder="Your 10th Board Marks*"
+                  required
+                />{" "}
+              </label>
+              <label for="text">
+                {" "}
+                <input
+                  type="text"
+                  name="_replyto"
+                  id="board"
+                  placeholder="Your 12th Board Marks*"
+                  required
+                />{" "}
+              </label>
+              <label>
+                {" "}
+                <textarea
+                  name="message"
+                  rows={6}
+                  placeholder="Why you need this Scholarship? Write in brief*"
+                ></textarea>
+              </label>
+              <button type="submit" value="Send">
+                Send
+              </button>
+            </form>
+          </div>
         </div>
-        <div className="contactFormContainer">
-          <form action="https://formspree.io/f/xgebelva" method="POST">
-            <div className="name">
-              <label for="text"> <input type="text" name="Name" id="name" placeholder="First Name*" required/></label>
-              <label for="text"> <input type="text" name="Name" id="name" placeholder="Last Name*" required/></label>
-            </div>
-            <label for="email"> <input type="email" name="_replyto" id="email" placeholder="Your Email*" required/> </label>
-            <label for="text"> <input type="text" name="_replyto" id="board" placeholder="Your 10th Board Marks*" required/> </label>
-            <label for="text"> <input type="text" name="_replyto" id="board" placeholder="Your 12th Board Marks*" required/> </label>
-            <label> <textarea name="message" rows={6} placeholder="Why you need this Scholarship? Write in brief*"></textarea>
-            </label>
-            <button type="submit" value="Send">Send</button>
-          </form>
-        </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
