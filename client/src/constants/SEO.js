0a3eb2c8d1167eb506b9c351679649aef33b8422
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-07-18 22:30:27 +0530

    all the constansts are stored here

---
+++ client/src/constants/SEO.js
@@ -0,0 +1,27 @@
+// Meta Data for SEO purposes :
+
+export const CommonPageHelmet = {
+  // Normal Tags :
+  description: "One Stop platform for women to excel their career in tech",
+  keywords: "Women, Career, Tech, AI-Assistance, Passwordless Authentication, One-2-One mentorship, Job Listings",
+
+  // Open Graph Tags :
+  ogTitle: "SheRise",
+  ogDescription: "One Stop platform for women to excel their career in tech",
+  ogImage: "https://ik.imagekit.io/sayancr777/tr:w-400/SheRise/Banner.jpg?updatedAt=1689688547059",
+  ogURL: "https://sherise.vercel.app/",
+  ogType: "website",
+  ogSiteName: "SheRise",
+  ogLocale: "en_US",
+};
+
+export const DynamicTitleHelmet = {
+
+  LandingPageTitle: "SheRise | Home",
+  ScholarshipPageTitle: "SheRise | Scholarship",
+  DevelopersPageTitle: "SheRise | Developers",
+  ContactPageTitle: "SheRise | Contact Us",
+  LoginPageTitle: "SheRise | Login",
+  RegisterPageTitle: "SheRise | Register",
+
+};
